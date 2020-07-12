2d93b290096627f8e0cf478b25c672826a0cbe33
PatrickKR <jehyeokchoe@gmail.com>
2020-07-12 13:47:06 +0900

    api to 1.0.4 copyright auto connect

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,30 @@
+/*
+ * Copyright (C) 2020 PatrickKR
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Contact me on <mailpatrickkr@gmail.com>
+ */
+
 plugins {
     kotlin("jvm") version "1.3.72"
     id("com.github.johnrengelman.shadow") version "6.0.0"
 }
 
 group = "com.github.patrick-mc"
-version = "1.0.3"
+version = "1.0.4"
 
 repositories {
     maven("https://repo.maven.apache.org/maven2/")
@@ -15,7 +35,7 @@ repositories {
 dependencies {
     compileOnly(kotlin("stdlib-jdk8"))
     compileOnly("org.spigotmc:spigot-api:1.8-R0.1-SNAPSHOT")
-    compileOnly("com.github.patrick-mc:websocket-client-api:1.0.3")
+    compileOnly("com.github.patrick-mc:websocket-client-api:1.0.4")
 }
 
 tasks {
--- src/main/kotlin/com/github/patrick/websocket/client/WebSocketClientCommand.kt
+++ src/main/kotlin/com/github/patrick/websocket/client/WebSocketClientCommand.kt
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2020 PatrickKR
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Contact me on <mailpatrickkr@gmail.com>
+ */
+
 package com.github.patrick.websocket.client
 
 import org.bukkit.command.Command
@@ -6,8 +26,7 @@ import org.bukkit.command.CommandSender
 import org.bukkit.command.TabCompleter
 
 class WebSocketClientCommand : CommandExecutor, TabCompleter {
-    private val instance =
-        WebSocketClientPlugin.INSTANCE
+    private val instance = WebSocketClientPlugin.INSTANCE
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         if (args.count() == 1) {
